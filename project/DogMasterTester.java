1d0a167b86b6c005687e3605e3b32da501268c93
Jonathan Damico <jj_damico@yahoo.com>
2015-10-03 23:27:28 -0700

    get rid of useless boolean (may need it later)

--- project/DogMasterTester.java
+++ project/DogMasterTester.java
@@ -7,8 +7,6 @@ public class DogMasterTester {
         Dog dog2 = new Dog ("Fido", 2, Dog.Breed.PITBULL, 693);
         Dog dog3 = new Dog ("Rover", 4, Dog.Breed.BORDER_COLLIE, 523);
         
-        boolean broken = false;
-        
         System.out.println(dog1.convertToHumanYears());
         System.out.println(dog1.convertToHumanYearsExact());
         System.out.println(dog1.convertToHumanYearsRounded());
